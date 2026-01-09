7fb0c5cbc5c067b4ca25cb831225cdf800d148fe
xevolesi <easytomalbolge@gmail.com>
2026-01-09 19:55:31 +0300

    Add conversion script for widerface

--- README.md
+++ README.md
@@ -12,9 +12,54 @@ I don't like `mmdetection`. From my point of view it's to overcomplicated. I pre
 
 # Notes and differencies
 
-- I use letter boxing during preprocessing for validation and inference. You can see more in `source/dataset/transforms`.
+## Augmentations
+I use letter boxing during preprocessing for validation and inference. You can see more in `source/dataset/transforms`.
+
+## Dataset
+
+I use custom dataset format. It consists of images and single CSV file. CSV file must have the following columns:
+```code
+image,boxes,key_points,subset
+```
+where
+- `image` is the relative path to the image;
+- `boxes` is the list of `[x1, y1, x2, y2]` boxes. Each column contains single list with multiple boxes, e.g. N faces on image = N boxes in list;
+- `key_points` is the list of facial key points in `[x, y, visibility]` format. Each column contains single list of lists of points, e.g. N faces on image = N boxes = N key points sets = N lists of lists with length 5 points (left eye, right eye, nose, left lip corner, right lib corner), each point is the list of 3 values `[x, y, visibility]`. `visibility` is the `visibility weight` that is `1` if this point in visible and `-1` otherwise. Note, that, during parsing (you can check `convert_widerface.py` script) `-1` visibility weight will be replaces to `0`. All questions about `labelv2` you can ask authors directly, since i don't know the rationale behind such visibility weight logic. As far as i remember guys from [insightface](https://github.com/deepinsight/insightface) use `labelv2` too; 
+- `subset` is the string with the subset name (`train` or `val`).
+
+To translate your WIDERFACE dataset to this custom format you need to perform the following steps:
+1. Download [WIDERFACE](http://shuoyang1213.me/WIDERFACE/);
+2. Unzip downloaded images in some directoryl
+2. Download [labelv2 folder](https://github.com/ShiqiYu/libfacedetection.train/tree/master/data/widerface/labelv2) and place it inside WIDERFACE folder.
+3. Run `convert_widerface.py` to create `.csv` file that will be saved into WIDERFACE folder.
+
+Conversion procedure is fairly simple, so i strongly encourage you to read the script and understand how the labels are parsed.
+
+Why i use such dataset format:
+- This is just a habit I acquired while participating in Kaggle competitions;
+- In my opinion, .csv is the most convenient format for storing labels in a single file.
+
+That's basically all, nothing special.
+
+Example:
+```code
+# Let 'D:\datasets\widerface' be the target folder;
+# Unzip train, val, test subsets of WIDERFACE into 'D:\datasets\widerface';
+# Place labelv2 folder into 'D:\datasets\widerface'.
+# Target structure:
+
+D:\datasets\widerface\:
+    | - labelv2
+    | - WIDER_test
+    | - WIDER_train
+    | - WIDER_val
+    | - widerface_main.csv # This .csv will be added by `convert_widerface.py` script
+```
+
+To train on your custom dataset you need to convert your dataset into the format described above. Just be sure that `image` column in CSV file consists of right relative paths to your images.
 
 # Results
+
 Author's results were taken from their training [repo](https://github.com/ShiqiYu/libfacedetection.train).
 
 Mine reults were calculated using `evaluate_widerface.py` script using original image size, `score_thresh=0.02` and `nms_thresh=0.45`.
---
+++ convert_widerface.py
@@ -0,0 +1,123 @@
+import argparse as ap
+import os
+import typing as ty
+from itertools import islice
+
+import pandas as pd
+
+WIDERFACE_SUBSETS = ("train", "val")
+
+
+def batched_custom(iterable, n):
+    """
+    Batches an iterable into chunks of size n.
+    Equivalent to itertools.batched in Python 3.12+.
+    """
+    if n < 1:
+        raise ValueError("n must be at least one")
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
+
+
+class AnnotationDict(ty.TypedDict):
+    image: str
+    boxes: list[int]
+    key_points: list[list[int]]
+
+
+class AnnotationContainer:
+    def __init__(
+        self, base_dir: str, anno_file_path: str, parse_kps: bool = False
+    ) -> None:
+        self.parse_kps = parse_kps
+        self.base_dir = base_dir
+        with open(anno_file_path, "r") as tf:
+            self.content = tf.read()
+        self.__parse()
+
+    def __parse(self) -> None:
+        self.meta = []
+        self.images = []
+        self.labels = []
+
+        samples_as_text = self.content.split("# ")[
+            1:
+        ]  # We skip 1 element since it's an empty string.
+        samples_as_text = [sample.strip().split("\n") for sample in samples_as_text]
+        for sample in samples_as_text:
+            image_meta, *boxes_str = sample
+
+            name, height, width = image_meta.split(" ")
+            image_path = os.path.join(
+                *name.split("/")
+            )  # This should be correct for Windows and Unix.
+            self.images.append(image_path)
+            self.meta.append((height, width))
+
+            labels = {"boxes": [], "key_points": []}
+            for i, point_set in enumerate(boxes_str):
+                coords = list(map(float, point_set.strip().split(" ")))
+
+                # Box is a first 4 coordinates in top_left_x, top_left_y, bottom_right_x, bottom_right_y format.
+                box = list(map(int, coords[:4]))
+
+                # Artificallly add class label to box.
+                box = [0, *box]
+                if any(coord < 0 for coord in box):
+                    msg = f"Image {image_path} has box with negative coords: {box}"
+                    raise ValueError(msg)
+                labels["boxes"].append(box)
+
+                # Key points are the rest points. It should be 5 in total, 3 components each (x, y, visibility flag).
+                if self.parse_kps:
+                    kps = []
+                    for point in batched_custom(coords[4:], 3):
+                        if all(coord == -1 for coord in point):
+                            kps.append([-1.0, -1.0, 0.0])
+                        else:
+                            point = list(point)
+                            point[-1] = 1.0
+                            kps.append(point)
+                    if len(kps) != 5:
+                        msg = f"Image {image_path} has more or less than 5 kps: {kps}"
+                        raise ValueError(msg)
+                    labels["key_points"].append(kps)
+
+            self.labels.append(labels)
+
+    def __len__(self) -> int:
+        return len(self.images)
+
+    def __getitem__(self, index: int) -> AnnotationDict:
+        image = self.images[index]
+        label = self.labels[index]
+        key_points = label["key_points"]
+        boxes = label["boxes"]
+        return {"image": image, "boxes": boxes, "key_points": key_points}
+
+    def __iter__(self):
+        for image_path, label in zip(self.images, self.labels):
+            yield {"image": image_path, **label}
+
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser()
+    parser.add_argument(
+        "--data_path", "-d", required=True, type=str, help="Path to WiderFace"
+    )
+    args = parser.parse_args()
+
+    datasets = []
+    for subset in WIDERFACE_SUBSETS:
+        anno_file_path = os.path.join(args.data_path, "labelv2", subset, "labelv2.txt")
+        image_dir = os.path.join(
+            args.data_path, f"WIDER_{subset}", f"WIDER_{subset}", "images"
+        )
+        container = AnnotationContainer(image_dir, anno_file_path, subset == "train")
+        dataframe = pd.DataFrame(data=container)
+        dataframe["subset"] = subset
+        datasets.append(dataframe)
+    dataset_df = pd.concat(datasets)
+    dataset_df_path = os.path.join(args.data_path, "widerface_main.csv")
+    dataset_df.to_csv(dataset_df_path, index=False)
